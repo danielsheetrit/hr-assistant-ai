6a352d8d2691a3c4e3bc0d2d8a34789fec153a43
danielsheetrit <danileshitrit022@gmail.com>
2023-04-19 16:43:46 +0300

    exclude some files

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ def chat():
     response = openai.ChatCompletion.create(
         messages=messages,
         model="gpt-3.5-turbo",
-        max_tokens=160,
+        max_tokens=200,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=1,
     )
     answer = response.choices[0].message.content.strip()
     messages.append({"role": "assistant", "content": f"{answer}"})
@@ -37,6 +37,3 @@ def chat():
 
 
 app.run()
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
